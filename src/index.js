2d1314a97286080583e7af8e150e13492ccda8b2
j363lu <j363lu@uwaterloo.ca>
2023-03-22 13:13:26 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import FileExplorer from './components/FileExplorer';
